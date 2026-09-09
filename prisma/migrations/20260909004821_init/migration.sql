14ca482b25d78917fc28cfabd67ac5a7437c285f
Leandro Soria Rosales <oncepla22@gmail.com>
2026-09-08 22:01:09 -0300

    feat: inicialice todo lo de prisma. no inclui .env

---
+++ prisma/migrations/20260909004821_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Autor" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "nacionalidad" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Autor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Libro" (
+    "id" SERIAL NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "autor" TEXT NOT NULL,
+    "anio" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Libro_pkey" PRIMARY KEY ("id")
+);
